78d51509011521a3caafd0c2e0bc64288199802f
ahnjungeun <616a65@gmail.com>
2022-06-02 01:07:36 +0900

    FEAT: 게임로직기초구성

--- team11app/urls.py
+++ team11app/urls.py
@@ -3,10 +3,11 @@ from team11app import views
 
 urlpatterns = [
     path('',views.index),
-    path('input/',views.input), # 끝에 /붙이기
+    path('create/',views.create), # 끝에 /붙이기
     # 2번째 인자로 view 정보 넣어줘야 함 
     # 아니면 에러남 TypeError: _path() missing 1 required positional argument: 'view'
-    path('show/<answer>', views.show), # <answer> 이름이 answer인 값이 가변적으로 들어옴
+    # path('show/<answer>', views.show), # <answer> 이름이 answer인 값이 가변적으로 들어옴
+    
 
 ]
 
--- team11app/views.py
+++ team11app/views.py
@@ -1,35 +1,72 @@
-from django.shortcuts import render,HttpResponse
-
+from django.shortcuts import render,HttpResponse, redirect
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
+nextId = 2
 inputNum = [
   {'id':1,'answer':'뭐라고 하지'}
 ]
 
-def HTMLTemplate():
-  global inputNum
-  inputList = ''
-  for userInput in inputNum:
-    inputList += f'<li>{userInput["id"]} {userInput["answer"]}</li>'
-  # f빼먹지 말아라...abs(x)
+def HTMLTemplate(content):
   return f'''
     <html>
       <body>
-        <h1>숫자야구</h1>
-        <p>입력한 숫자:</p>
-        <ul>
-          {inputList}
-        </ul>
+        <h1><a href="/">숫자야구</a></h1>
+        {content}
       </body>
     </html>
-
   '''
 
+
 def index(request): # '''긴 문자열 입력'''
-  return HttpResponse(HTMLTemplate())
+  start = '<a href="/create/">시작하기</a>'
+  return HttpResponse(HTMLTemplate(start))
 
-def input(request):
-  return HttpResponse('숫자입력')
 
-def show(request,answer): # url에서 show/answer로 들어온 값을 인자로
-  return HttpResponse('결과보여주기'+answer)
+@csrf_exempt
+def create(request):
+
+  global inputNum
+  inputList = ''
+  if inputNum != None:
+    for userInput in inputNum:
+      inputList += f'<li>{userInput["id"]} {userInput["answer"]}</li>'
+  intro = f'''
+      <p>입력한 숫자:</p>
+      {inputList}
+    '''
+
+  answerNumber = 123
+
+  global nextId
+  if request.method == 'GET':
+    article = '''
+        <form action="/create/" method="post">
+            <p><input type="text" name="gameNum" placeholder="title"></p>
+            <p><input type="submit"></p>
+        </form>
+    '''
+    return HttpResponse(HTMLTemplate(intro+article))
+  elif request.method == 'POST':
+    gameNum = int(request.POST['gameNum'])
+    newInputNum = {"id":nextId, "answer":gameNum}
+    inputNum.append(newInputNum)
+    if answerNumber == gameNum:
+      print("정답")
+      return redirect('/')
+    url = '/create/'
+    nextId = nextId + 1
+    return redirect(url)
+
+
+
+
+
+
+
+
+
+
+
+# def show(request,answer): # url에서 show/answer로 들어온 값을 인자로
+#   return HttpResponse('결과보여주기'+answer)
